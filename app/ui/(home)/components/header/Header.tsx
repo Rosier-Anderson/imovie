c505eccd1dd9e3513ff38cc1d015714f69de6552
Anderson <97542626+Rosier-Anderson@users.noreply.github.com>
2025-07-27 04:36:38 +0000

    chore: comment out mobile resize detection and logging logic

--- app/ui/(home)/components/header/Header.tsx
+++ app/ui/(home)/components/header/Header.tsx
@@ -6,20 +6,20 @@ import SearchInput from "../Search/SearchInput";
 import Login from "@/app/(auth)/login/page";
 
 export default function Header() {
-  const [showOnMobile, setShowOnMobile] = useState<boolean>(false);
-  useEffect(() => {
-    const handleShowOnMobile = (): void => {
-      const vw = window.innerWidth;
-      setShowOnMobile(vw >= 768);
-    };
-    handleShowOnMobile();
-    window.addEventListener("resize", handleShowOnMobile);
-    return () => window.removeEventListener("resize", handleShowOnMobile);
-  }, []);
-  // just log to the console
-  useEffect(() => {
-    console.log(showOnMobile);
-  }, [showOnMobile]);
+  // const [showOnMobile, setShowOnMobile] = useState<boolean>(false);
+  // useEffect(() => {
+  //   const handleShowOnMobile = (): void => {
+  //     const vw = window.innerWidth;
+  //     setShowOnMobile(vw >= 768);
+  //   };
+  //   handleShowOnMobile();
+  //   window.addEventListener("resize", handleShowOnMobile);
+  //   return () => window.removeEventListener("resize", handleShowOnMobile);
+  // }, []);
+  // // just log to the console
+  // useEffect(() => {
+  //   console.log(showOnMobile);
+  // }, [showOnMobile]);
   return (
     <div className="w-full border-b h-20">
       <header className=" flex justify-between items-center max-w-[1200px] h-full mx-auto ">
